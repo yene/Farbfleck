ff9c74f44e5f1dba3ecabfa787bc11591465499b
yene <yene@cocoadojo.com>
2013-11-11 16:47:15 +0100

    improved title tag on starting page

--- theme/header.php
+++ theme/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title><?php wp_title(); ?></title>
+  <title><?php bloginfo('name'); ?><?=is_front_page() ? "":  wp_title() ; ?></title>
   <meta name="description" content="<?php bloginfo( 'description' ); ?>">
   <!-- Normalize.css is a customisable CSS file that makes browsers render all elements more consistently and in line with modern standards. -->
   <link rel="stylesheet" media="screen" href="normalize.css">
